05c33cc402b8160ea4f1a2b4d4c49f3049b2e956
reinheimermat <mreinheimer0@gmail.com>
2024-05-26 11:52:23 -0300

    feat: add date formatted

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,15 +1,34 @@
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { useNavigate } from 'react-router-dom'
+
 interface PostProps {
   title: string
   body: string
-  date: string
+  createdAt: string
+  id: number
 }
 
-export function Post({ title, body, date } = {} as PostProps) {
+export function Post({ title, body, createdAt, id }: PostProps) {
+  const navigate = useNavigate()
+
+  function handleViewPost() {
+    navigate(`/posts/${id}`)
+  }
+
   return (
-    <div className="max-h-64 rounded-xl bg-basePost p-8">
+    <div
+      className="max-h-64 rounded-xl bg-basePost p-8"
+      onClick={handleViewPost}
+    >
       <div className="mb-5 flex items-start justify-between">
         <h1 className="text-xl font-bold text-baseTitle">{title}</h1>
-        <span className="text-nowrap text-sm text-baseSpan">{date}</span>
+        <span className="text-nowrap text-sm text-baseSpan">
+          {formatDistanceToNow(new Date(createdAt), {
+            addSuffix: true,
+            locale: ptBR,
+          })}
+        </span>
       </div>
       <p className="max-h-28 overflow-hidden text-ellipsis">{body}</p>
     </div>
